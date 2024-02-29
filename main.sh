fe902678e04554ff5ae30a28f74fda6cbe402163
Misael M. Morales <misaelmiguelmorales@gmail.com>
2024-02-29 00:27:30 -0600

    update
    .keras model with (batch,None,channels) such that transfer learning logs can be read despite different lengths

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the main script and save outputs (.out) and errors (.err)
-python3 auto_baseline_correction.py > auto_blc.out 2> auto_blc.err
+python3 auto_baseline_correction.py 2>&1 | tee out.txt
