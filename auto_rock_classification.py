1358bd78ba2833f69eac057e9d4b35d864c194dd
Misael M. Morales <misaelmiguelmorales@gmail.com>
2024-03-01 22:37:50 -0600

    arc

--- auto_rock_classification.py
+++ auto_rock_classification.py
@@ -25,7 +25,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans
